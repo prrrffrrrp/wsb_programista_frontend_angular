0cbdc379ca1494c458649e1b4e6140c6090b851f
arnau <arnau.vidal@gigaset.com>
2021-02-07 14:05:46 +0100

    Zjazd5: RWD (grid), git (pull requests, branches...), NodeJs

--- zjazd_5/node/app.js
+++ zjazd_5/node/app.js
@@ -2,7 +2,7 @@ const functions = require("./functions")
 
 const express = require("express");
 const path = require("path")
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.set("view engine", "hbs")
